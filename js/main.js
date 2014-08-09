1bf0713246199a4b557a1ffc9b9ba4b06447a516
Zachary Lincoln <zach@burlingtoncollective.com>
2014-08-08 20:35:18 -0400

    fixed affix - ready for more templates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(function(){
 	$('#sub-nav').affix({
 		offset: {
-			top: 109
+			top: 132
 		}
+	}).on('affix.bs.affix', function(){
+		$('header + section').css('padding-top', $('#sub-nav').outerHeight()+'px');
+	}).on('affix-top.bs.affix', function(){
+		$('header + section').css('padding-top', '0px');
 	});
 });
