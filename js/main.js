79166bb325d8858acfc787773e44743e800ef9ac
Zachary Lincoln <zach@burlingtoncollective.com>
2014-08-09 20:19:16 -0400

    carousel upgrades

--- js/main.js
+++ js/main.js
@@ -8,4 +8,20 @@ $(function(){
 	}).on('affix-top.bs.affix', function(){
 		$('header + section').css('padding-top', '0px');
 	});
+
+	$('[data-fitvids-target]').fitVids();
+
+	$('.carousel')
+		.on('slide.bs.carousel', function(e){
+			console.log(e.relatedTarget);
+			var newHeight = $(e.relatedTarget).outerHeight();
+			console.log(newHeight);
+			$(this).find('.item.active').parent().animate({
+				'height': newHeight
+			}, 500);
+		})
+		.carousel({
+			interval: 10000
+		});
+
 });
